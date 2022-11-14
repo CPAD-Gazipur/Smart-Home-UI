dc0b511d0123b97c25e7dbfeea5418845de1cf44
alamin-karno <amin15-1951@diu.edu.bd>
2022-11-14 21:42:21 +0600

    Smart Home UI: Initialize

---
+++ android/app/src/main/kotlin/com/alaminkarno/smart_home_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alaminkarno.smart_home_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home_ui/view/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Home UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
